0344bd30b3fa59f9c914d63f4b183e5e17191b44
Fatima Ebrahim Ali Abdulla <83508576+fa1819@users.noreply.github.com>
2025-05-11 22:34:01 +0000

    e

---
+++ htdocs/db.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+  	<title>PHP + MySQL</title>
+  	<meta charset="UTF-8">
+  </head>
+  <body>
+    <?php
+    $host = "127.0.0.1";  // localhost also works sometimes
+    $dbname = getenv("db_name");
+    $user = getenv("db_user");
+    $pass = getenv("db_pass");
+
+    try {
+        $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "<p>Connected to database!</p>";
+    } catch (PDOException $e) {
+        echo "<p>Connection failed: " . $e->getMessage() . "</p>";
+    }
+    ?>
+  </body>
+</html>
